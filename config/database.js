cfb9bce1898e974ba0065d5135cdbf7fb419393f
theuniquecreater-sys <theuniquecreater@gmail.com>
2026-04-13 19:34:23 +0530

    Create database configuration file

---
+++ config/database.js
@@ -0,0 +1,9 @@
+// Database configuration
+const dbConfig = {
+    host: 'localhost',
+    user: 'root',
+    password: 'your_password',
+    database: 'your_database'
+};
+
+module.exports = dbConfig;
