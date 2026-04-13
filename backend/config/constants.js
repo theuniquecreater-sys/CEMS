66ac2483291f0b914739a950db5bc2f2cec14cef
theuniquecreater-sys <theuniquecreater@gmail.com>
2026-04-13 19:09:42 +0530

    Create backend/config/constants.js file with application constants for CEMS project

---
+++ backend/config/constants.js
@@ -0,0 +1,9 @@
+'use strict';
+
+module.exports = {
+    API_BASE_URL: 'https://api.cems.com',
+    DB_CONNECTION_STRING: 'mongodb://localhost:27017/cems',
+    PORT: 3000,
+    ENVIRONMENT: 'development',
+    JWT_SECRET: 'your_jwt_secret',
+};
